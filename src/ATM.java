d01bd753aa2e3f88bcf506e3db09621d290f9644
DrScott <dougscott2@gmail.com>
2015-10-09 15:37:58 -0400

    Solid 2 class build

--- src/ATM.java
+++ src/ATM.java
@@ -6,24 +6,15 @@ import java.util.Scanner;
 public class ATM {
     static Account account;
 
-    public static void run() throws Exception {
 
+    public static void run() throws Exception {
+        account = new Account();
+
+        account.login();
 
         while (true) {
-            account.login();
-            account.mainMenu();
-            String option = ATM.nextLine();  //stores input as a string
-            int optionNum = Integer.valueOf(option); //converts string into an int
-            if (optionNum == 1) {
-                account.checkBalance();
-            } else if (optionNum == 2) {
-                account.withdraw();
-            } else if (optionNum == 3) {
-                account.exit();
-            } else {
-                throw new Exception("Invalid selection!");
-            }
 
+            account.mainMenu();
 
         }
     }
@@ -32,7 +23,7 @@ public class ATM {
     static String nextLine() {  //static method to get next line input so i don't have to create it a bunch of times
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        return nextLine();
+        return s;
     }
 }
 
--- src/Account.java
+++ src/Account.java
@@ -5,56 +5,71 @@ import java.util.Scanner;
  * Created by DrScott on 10/9/15.
  */
 public class Account {
-    double balance;
+    HashMap< String , Double> accounts = new HashMap();
     String name;
-    HashMap< String, Double> accounts = new HashMap();
-
+    double balance;
     void login() throws Exception {
+
         System.out.println("Welcome to the bank. Please enter your name");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        if( accounts.get(name).equals(false)){
+        String accountName = scanner.nextLine();
+        name = accountName;
+
+        if (!accounts.containsKey(accountName)) {
             System.out.println("Account not found! Do you want to make a new account? y/n");
             String s = ATM.nextLine();
-            if(s.equals("y")){
+            if (s.equals("y")) {
                 System.out.println("How much are you depositing?");
-                 s = ATM.nextLine();
+                s = ATM.nextLine();
                 balance = Double.parseDouble(s);
-                accounts.put(name, balance);
+                accounts.put(accountName, balance);
+            } else if (s.equals("n")) {
+                System.out.println("K, well...bye!");
+                System.exit(0);
+
+            } else if (accountName.equals("")) {
+                throw new Exception("Invalid entry");
             }
         }
-        if (name.equals("")) {
-            throw new Exception("Invalid entry");
-        }
     }
+
     void mainMenu() throws Exception {
-        System.out.println("Welcome, " + name + ". What would you like to do?");
+        System.out.println("Welcome, " + accounts.get(name) + ". What would you like to do?");
         System.out.println("[1] Check balance");
         System.out.println("[2] Withdraw from account");
         System.out.println("[3] Exit");
-        String option = ATM.nextLine();
-    }
-    void checkBalance() {
-        System.out.println("Your balance is $"+ accounts.get(name));
-    }
-    void withdraw() throws Exception {
-        System.out.println("How much would you like to withdraw?");
+        System.out.println("[4] Delete account");
+        String option = ATM.nextLine();  //stores input as a string
+        int optionNum = Integer.valueOf(option); //converts string into an int
 
-        String withdraw = ATM.nextLine();
+             if (optionNum == 1) {
+            System.out.println("Your balance is $" + accounts.get(name));
+             } else if (optionNum == 2) {
 
-        int withdrawNum = Integer.valueOf(withdraw);
+                 System.out.println("How much would you like to withdraw?");
 
-        if (withdrawNum <= 100) {
-            balance -= withdrawNum;
-            System.out.println("You've withdrawn $" + withdrawNum);
-            System.out.println("Your new balance is $" + balance);
-        } else if (withdrawNum > 100 || withdrawNum < 0) {
-            throw new Exception("You don't have that much money");
-        }
-    }
-    void exit(){
-        System.out.println("Thanks for stopping by, have a nice day");
-        System.exit(0);
+                     String withdraw = ATM.nextLine();
+
+                  double withdrawNum = Double.parseDouble(withdraw);
+
+                  if (withdrawNum <= accounts.get(name)) {
+                  double balance = accounts.get(name) - withdrawNum;
+                  System.out.println("You've withdrawn $" + withdrawNum);
+                 System.out.println("Your new balance is $" + balance);
+                 //accounts.replace(name, balance, name, balance);
+            } else if (withdrawNum > accounts.get(name)) {
+                throw new Exception("You don't have that much money");
+            }
+        } else if (optionNum == 3) {
+
+            System.out.println("Thanks for stopping by, have a nice day");
+            System.exit(0);
+        } else if (optionNum == 4) {
+                 System.out.println("Fine, be that way, " + name);
+                 accounts.remove(name);
+                 System.exit(0);
+
+             }
     }
 
 }
